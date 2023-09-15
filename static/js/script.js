ac45de98eded98f8fae3e175f5a46a28e0dacad9
EdwinGc03 <edwinalancg03@gmail.com>
2023-09-15 13:59:59 -0600

    f

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,28 @@ document.addEventListener('click', (e) => {
         menu.classList.remove('active');
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openModalLink = document.getElementById('openModalLink');
+    const modal = document.getElementById('myModal');
+    const closeModal = document.querySelector('.close');
+
+    // Agrega un evento clic para abrir la ventana emergente
+    openModalLink.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    // Agrega un evento clic para cerrar la ventana emergente
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Cierra la ventana emergente si se hace clic fuera de ella
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
+
